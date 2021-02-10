a12112ed47c0dfe64c248de38142d6d14651dd1c
AliyaZamir <69330907+AliyaZamir@users.noreply.github.com>
2021-02-10 16:51:04 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,25 @@
+var hour;
+var second;
+var minute;
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
 }
 
 function draw() {
-  background(255,255,255);  
+  background(255,255,255);
+  
+  text('Current hour:\n' + hour, 5, 50);
+  text('Current minute: \n' + minute, 80, 50);
+  text('Current second: \n' + second, 165, 50);
+
+  angleMode(DEGREES);
+  map()
+  scAngle = map(sc, 0, 60, 0, 360);
+
+  stroke(250,0,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+
   drawSprites();
 }
